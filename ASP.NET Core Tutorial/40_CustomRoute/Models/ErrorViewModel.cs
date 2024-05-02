957736fba06c89de2bccaa2aef6dbaff0427f116
CeydaKesgin5 <ceydakesgin778@gmail.com>
2024-05-02 21:16:47 +0300

    Initial commit

---
+++ ASP.NET Core Tutorial/40_CustomRoute/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _40_CustomRoute.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
